18a5ab32de447f62d0bac72fe8be90fbe0e353a4
Zhafirah Rizqy Nur <ZPrasetija@gmail.com>
2022-02-12 11:47:28 +0700

    klinik

---
+++ home.php
@@ -0,0 +1,11 @@
+<b>
+    <font color="#00b994">DASHBOARD</font>
+</b>
+<br><br>
+<hr color="#00b994" size="2px" width="100%">
+<br>
+<font size="+3"><b>Test</b></font><br><br>
+<font color="#646464" size="+1">
+    <p><b>Sistem Informasi Klinik</b> Ini adalah sistem informasi klinik.</p>
+</font>
+<br>
